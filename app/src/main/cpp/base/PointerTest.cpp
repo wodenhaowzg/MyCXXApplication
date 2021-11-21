0b9c7e27db60b75e83d6b0cf1892b6c361c61ec7
Zane.Wang <>
2021-11-22 01:53:45 +0800

    wzg up

--- app/src/main/cpp/base/PointerTest.cpp
+++ app/src/main/cpp/base/PointerTest.cpp
@@ -3,38 +3,59 @@
 //
 #include "object/BaseObject.h"
 
+void createPointer();
+
 extern "C" JNIEXPORT void JNICALL Java_com_example_mycxxapplication_jni_PointerTest_pointerTest(JNIEnv *env, jobject thiz) {
-    stringstream ss;
+    // 创建指针
+    createPointer();
+
+//    stringstream ss;
+//    // 定义一个变量
+//    int a = 100;
+//    // 定义一个指针变量，并将变量 a 的地址值赋值给 b
+//    int *b = &a;
+//    ss << "a address value: " << b << " | a value: " << *b;
+////    LOGI(BASETAG, ss.str().c_str());
+//    ss.clear();
+//    ss.str("");
+//
+//    // 定义一个数组
+//    int var1[] = {100, 200};
+//    // 定义一个指针数组
+//    int *pointer_array[2];
+//    // 定义一个数组指针
+//    int (*pArray)[2];
+//
+//    // 将 var1 的地址值赋值给 var2
+//    pArray = &var1;
+//    LOGI(BASETAG, "打印数组指针");
+//    for (int i = 0; i < 2; ++i) {
+//        LOGI(BASETAG, "value: %d", (*pArray)[i]);
+//    }
+//
+//    // 将 var 数组中每个值的内存地址值赋值给 pointer_array 指针数组
+//    for (int i = 0; i < 2; ++i) {
+//        pointer_array[i] = &(var1[i]);
+//    }
+//
+//    LOGI(BASETAG, "打印指针数组");
+//    for (int i = 0; i < 2; ++i) {
+//        LOGI(BASETAG, "value : %d", (*pointer_array)[i]);
+//    }
+//
+////    char charArray[] = {"helloworld"};
+////    char* charStr = "helloworld";
+////    charArray = charStr;
+}
+
+void createPointer() {
     // 定义一个变量
     int a = 100;
-    // 定义一个指针变量，并将变量 a 的地址值赋值给 b
-    int *b = &a;
-    ss << "a address value: " << b << " | a value: " << *b;
-//    LOGI(BASETAG, ss.str().c_str());
-    ss.clear();
-    ss.str("");
-
-    // 定义一个数组
-    int var1[] = {100, 200};
-    // 定义一个指针数组
-    int *pointer_array[2];
-    // 定义一个数组指针
-    int (*pArray)[2];
-
-    // 将 var1 的地址值赋值给 var2
-    pArray = &var1;
-    LOGI(BASETAG, "打印数组指针");
-    for (int i = 0; i < 2; ++i) {
-        LOGI(BASETAG, "value: %d", (*pArray)[i]);
-    }
-
-    // 将 var 数组中每个值的内存地址值赋值给 pointer_array 指针数组
-    for (int i = 0; i < 2; ++i) {
-        pointer_array[i] = &(var1[i]);
-    }
-
-    LOGI(BASETAG, "打印指针数组");
-    for (int i = 0; i < 2; ++i) {
-        LOGI(BASETAG, "value : %d", (*pointer_array)[i]);
-    }
+    int b = 200;
+    // 定义一个指针，并将变量赋值给指针。
+    int *pointer = &a;
+    LOGI(BASETAG, "将变量 a 赋值给指针 pointer ，pointer 的值 = %p ，值里面的内容 = %d ，pointer 自身的地址 = %p", pointer, *pointer, &pointer);
+    // 再次改变指针的值
+    pointer = &b;
+    LOGI(BASETAG, "将变量 b 赋值给指针 pointer ，pointer 的值 = %p ，值里面的内容 = %d ，pointer 自身的地址 = %p", pointer, *pointer, &pointer);
 }
--- app/src/main/cpp/base/object/ObjectTest.cpp
+++ app/src/main/cpp/base/object/ObjectTest.cpp
@@ -31,12 +31,23 @@ void transObj1(BaseObject object);
 
 void transObj2(BaseObject *pObject);
 
+/**
+ * 结论
+ * 1、对象可以在栈和堆两中内存空间上创建。其中在栈上创建的对象，其生命周期由系统管理，而在堆上创建的生命周期，需要自己管理，new/delete
+ * 2、对象在方法之间传递时，引用传递会自动生成一个新的临时对象，指针传递则不会自动生成临时对象。
+ * 3、在栈中创建的对象，其地址不能被改变、转移。
+ */
+void changeObject();
+
 extern "C" JNIEXPORT void JNICALL Java_com_example_mycxxapplication_jni_ObjectTest_structTest(JNIEnv *env, jobject thiz) {
     // 1、对象的创建和释放。
     createObject();
     LOGD(BASETAG, "========================================分隔符================================");
     // 2、对象的传递。
     transObject();
+    LOGD(BASETAG, "========================================分隔符================================");
+    // 3、对象的改变
+    changeObject();
 }
 
 void createObject() {
@@ -59,6 +70,19 @@ void transObject() {
     LOGD(BASETAG, "我是在堆空间上创建的obj2，地址是=%p", obj2);
     transObj2(obj2);
     delete obj2;
+}
+
+void changeObject() {
+    BaseObject obj1;
+    LOGD(BASETAG, "我是在栈空间上创建的obj1，地址是=%p", &obj1);
+    BaseObject* obj2 = new BaseObject();
+    LOGD(BASETAG, "我是在堆空间上创建的obj2，地址是=%p", obj2);
+    BaseObject* obj3 = new BaseObject();
+    LOGD(BASETAG, "我是在堆空间上创建的obj3，地址是=%p", obj3);
+    obj1 = *obj2;
+    LOGD(BASETAG, "将 obj2 赋值给 obj1 后，obj1 地址是=%p，obj2 地址是=%p", &obj1, obj2);
+    obj2 = obj3;
+    LOGD(BASETAG, "将 obj3 赋值给 obj2 后，obj2 地址是=%p，obj3 地址是=%p", obj2, obj3);
 }
 
 void transObj1(BaseObject obj1) {
